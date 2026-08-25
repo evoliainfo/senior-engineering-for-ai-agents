ac448313cf8ec87a90a0644661578010c1665527
Evolia <evolia.info@gmail.com>
2026-08-25 09:15:32 +0200

    feat(m4): expose Codex inventory adapter public API

--- tool_capabilities/__init__.py
+++ tool_capabilities/__init__.py
@@ -15,6 +15,21 @@ from .core import (
     validate_document,
     validate_resolution,
 )
+from .codex_adapter import (
+    ADAPTER_REPORT_SCHEMA_ID,
+    BINDING_KINDS,
+    HARNESS_KIND,
+    INVENTORY_SCHEMA_ID,
+    CodexInventoryError,
+    adapt_inventory,
+    validate_adapter_report,
+    validate_inventory,
+)
+from .codex_bridge import (
+    BRIDGE_SCHEMA_ID,
+    resolve_codex_inventory,
+    validate_bridge_report,
+)
 
 __all__ = [
     "ACCESS",
@@ -30,4 +45,15 @@ __all__ = [
     "resolve",
     "validate_document",
     "validate_resolution",
+    "ADAPTER_REPORT_SCHEMA_ID",
+    "BINDING_KINDS",
+    "HARNESS_KIND",
+    "INVENTORY_SCHEMA_ID",
+    "CodexInventoryError",
+    "adapt_inventory",
+    "validate_adapter_report",
+    "validate_inventory",
+    "BRIDGE_SCHEMA_ID",
+    "resolve_codex_inventory",
+    "validate_bridge_report",
 ]
