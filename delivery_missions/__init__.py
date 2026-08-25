4e4c1b73b31bb95b355890e89e661637fb85a57c
Evolia <evolia.info@gmail.com>
2026-08-25 10:56:22 +0200

    feat(m5): export evidence acceptance surface

--- delivery_missions/__init__.py
+++ delivery_missions/__init__.py
@@ -2,20 +2,36 @@
 
 from .launch_production_web_product import (
     DECISION_SCHEMA_ID,
+    EVIDENCE_RECEIPT_SCHEMA_ID,
+    EXECUTION_RESULT_SCHEMA_ID,
     MISSION_SCHEMA_ID,
     MissionError,
+    MissionEvidenceError,
+    advance_from_execution,
     decide_next_action,
+    evaluate_execution_result,
     initialize_project_state,
+    seal_execution_result,
     validate_decision,
+    validate_evidence_receipt,
+    validate_execution_result,
     validate_spec,
 )
 
 __all__ = [
     "DECISION_SCHEMA_ID",
+    "EVIDENCE_RECEIPT_SCHEMA_ID",
+    "EXECUTION_RESULT_SCHEMA_ID",
     "MISSION_SCHEMA_ID",
     "MissionError",
+    "MissionEvidenceError",
+    "advance_from_execution",
     "decide_next_action",
+    "evaluate_execution_result",
     "initialize_project_state",
+    "seal_execution_result",
     "validate_decision",
+    "validate_evidence_receipt",
+    "validate_execution_result",
     "validate_spec",
 ]
